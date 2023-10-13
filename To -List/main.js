5d6522c57e86152ba345f165fdbebfbbf50b9794
Shafaullah Malik <72027876+MuhammadShafaullah@users.noreply.github.com>
2023-10-13 16:46:38 +0500

    Add item

---
+++ To -List/main.js
@@ -0,0 +1,31 @@
+const ary = [];
+
+console.log(ary)
+
+const save=document.getElementById("save");
+const data=document.getElementById("data");
+const result=document.getElementById("result");
+
+
+save.addEventListener("click",function(){
+
+     
+    ary.push(data.value)
+    data.value=''
+    let html = "";
+
+ary.forEach(function(ary){
+ html += ` <div class=""> ${ary}
+              
+ </div>  `
+   
+    
+})
+result.innerHTML=html
+    
+})
+
+
+
+
+
